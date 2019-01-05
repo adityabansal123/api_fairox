c15b43a3df65f3ef6fd15e1fafc69b100cb02aa9
Aditya Bansal <eadityabansal@gmail.com>
2019-01-05 19:58:07 +0530

    testing auth

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -13,20 +13,20 @@ class TestController extends Controller
     /**
      * {@inheritdoc}
      */
-//    public function behaviors()
-//    {
-//            $behaviors = parent::behaviors();
-//            $behaviors['authenticator'] = [
-//                'class' => HttpBasicAuth::className(),
-//                'auth' => function($username, $password){
-//                    $user = User::findByUsername($username);
-//                    if($user && $user->validatePassword($password, $user->password)){
-//                        return $user;
-//                    }
-//                }
-//            ];
-//            return $behaviors;
-//    }
+    public function behaviors()
+    {
+            $behaviors = parent::behaviors();
+            $behaviors['authenticator'] = [
+                'class' => HttpBasicAuth::className(),
+                'auth' => function($username, $password){
+                    $user = User::findByUsername($username);
+                    if($user && $user->validatePassword($password, $user->password)){
+                        return $user;
+                    }
+                }
+            ];
+            return $behaviors;
+    }
 
     public function actionIndex(){
         return 'hello';
