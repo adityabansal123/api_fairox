fbf39f66894379c543eb100ead030073952d0282
Aditya Bansal <eadityabansal@gmail.com>
2019-01-05 19:45:50 +0530

    testing test controller 5

--- config/web.php
+++ config/web.php
@@ -57,8 +57,11 @@ $config = [
             'rules' => [
                 [
                     'class' => 'yii\rest\UrlRule',
-                    'controller' => 'Test'
-                ]
+                    'controller' => ['test/index']
+                ],
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
             ],
         ],
 
