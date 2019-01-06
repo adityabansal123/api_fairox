da9b93cb61c8e5d8f5c4c2a959b0d8888c203d8c
Aditya Bansal <eadityabansal@gmail.com>
2019-01-06 12:43:11 +0530

    testing auth 17

--- controllers/OauthController.php
+++ controllers/OauthController.php
@@ -12,15 +12,16 @@ class OauthController extends Controller{
         $model = new UserSignup();
         if($model->load(\Yii::$app->getRequest()->getBodyParams(), '') && $model->validate()){
             $user = new User();
-            $user->username = $this->username;
-            $user->email = $this->email;
+            $user->username = $model->username;
+            $user->email = $model->email;
             $user->access_token = \Yii::$app->security->generateRandomString(32);
             $user->unique_key = time().mt_rand(10,99);
             $user->created_at = date('Y-m-d H:i:s', strtotime('now'));
-            $user->setPassword($this->password);
+            $user->setPassword($model->password);
             $user->generateAuthKey();
             return ($user->save()) ? $user : 'Failed to save';
         }
+        return $model;
     }
     public function actionLogin(){
         $model = new LoginForm();
