9c23571db76483cfe20083f50ad96f0528a5126d
Aditya Bansal <eadityabansal@gmail.com>
2019-01-14 13:14:24 +0530

    testing auth 25

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -15,24 +15,25 @@ class TestController extends Controller
     /**
      * {@inheritdoc}
      */
-    public function behaviors()
-    {
-            $behaviors = parent::behaviors();
-            $behaviors['authenticator'] = [
-//                'class' => HttpBasicAuth::className(),
-//                'auth' => function($username, $password){
-//                    $user = User::findByUsername($username);
-//                    if($user && $user->validatePasswordBasic($password, $user->password)){
-//                        return $user;
-//                    }
-//                }
-                'class' => HttpBearerAuth::className()
-
-            ];
-            return $behaviors;
-    }
+//    public function behaviors()
+//    {
+//            $behaviors = parent::behaviors();
+//            $behaviors['authenticator'] = [
+////                'class' => HttpBasicAuth::className(),
+////                'auth' => function($username, $password){
+////                    $user = User::findByUsername($username);
+////                    if($user && $user->validatePasswordBasic($password, $user->password)){
+////                        return $user;
+////                    }
+////                }
+//                'class' => HttpBearerAuth::className()
+//
+//            ];
+//            return $behaviors;
+//    }
 
     public function actionIndex(){
+        return Yii::$app->request->getHeaders();
         return 'hello';
     }
 
